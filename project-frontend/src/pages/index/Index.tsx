9e8b3accd85d43a23f2ddeff111683d4df1b1986
beeeeeeen9677 <benlau1234567@gmail.com>
2025-11-17 19:18:40 +0800

    added  index container template

--- project-frontend/src/pages/index/Index.tsx
+++ project-frontend/src/pages/index/Index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import Header from "../../components/Header";
 
 // type IndexProps = {
@@ -10,7 +11,32 @@ function Index() {
     <div className="animate-fade-in min-h-screen">
       <title>Premium Ben Toys</title>
       <Header />
-      index
+      New Arrivals
+      {/* New Arriavals */}
+      <div className="flex gap-4 p-4 bg-black dark:bg-gray-600">
+        {/* Goods */}
+        <Link to={""} className="w-40 h-80 bg-white">
+          <img src={""} className="bg-amber-200 w-full h-1/2"></img>
+          <div className="text-black font-bold text-xl">Title</div>
+        </Link>
+        <Link to={""} className="w-40 h-80 bg-white">
+          <img src={""} className="bg-amber-200 w-full h-1/2"></img>
+          <div className="text-black font-bold text-xl">Title</div>
+        </Link>
+      </div>
+      Closing Soon
+      {/* Closing Soon */}
+      <div className="flex gap-4 p-4 bg-black dark:bg-gray-600">
+        {/* Goods */}
+        <Link to={""} className="w-40 h-80 bg-white">
+          <img src={""} className="bg-amber-200 w-full h-1/2"></img>
+          <div className="text-black font-bold text-xl">Title</div>
+        </Link>
+        <Link to={""} className="w-40 h-80 bg-white">
+          <img src={""} className="bg-amber-200 w-full h-1/2"></img>
+          <div className="text-black font-bold text-xl">Title</div>
+        </Link>
+      </div>
     </div>
   );
 }
