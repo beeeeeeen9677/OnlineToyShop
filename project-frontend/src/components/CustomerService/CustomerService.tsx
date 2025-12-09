63f4ddc6d09688b3a4369d4454c0b2b3e27c35a9
beeeeeeen9677 <benlau1234567@gmail.com>
2025-12-09 15:06:02 +0800

    Update CustomerService.tsx

--- project-frontend/src/components/CustomerService/CustomerService.tsx
+++ project-frontend/src/components/CustomerService/CustomerService.tsx
@@ -58,7 +58,7 @@ function CustomerService() {
         )
       );
 
-      console.log("Received message:", data);
+      //console.log("Received message:", data);
 
       // queryClient.invalidateQueries({ queryKey: chatRoomsQueryKey });
       // queryClient.invalidateQueries({
