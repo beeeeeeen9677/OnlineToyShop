423895c4a805c62a06e85edc99e8ebcf3c593088
beeeeeeen9677 <benlau1234567@gmail.com>
2025-12-22 09:21:23 +0800

    Adjusted SearchItem style

--- project-frontend/src/pages/search/SearchItem.tsx
+++ project-frontend/src/pages/search/SearchItem.tsx
@@ -17,13 +17,13 @@ function SearchItem({ itemDetails }: SearchItemProps) {
       <div className="w-full h-7/11 sm:h-8/11 md:h-2/3 overflow-hidden items-center flex ">
         <img
           src={itemDetails.imageUrl}
-          className="object-contain scale-200 transform translate-y-12 group-hover:scale-100 group-hover:translate-0 transition duration-300"
+          className="object-contain aspect-square scale-200 transform translate-y-12 group-hover:scale-100 group-hover:translate-0 transition duration-300"
         />
       </div>
 
-      <div className="font-oswald px-1 pt-1 text-black font-black text-sm line-clamp-2">
+      <h1 className="font-oswald px-1 pt-1 text-black font-black text-md line-clamp-3">
         {itemDetails.name}
-      </div>
+      </h1>
       <div className=" px-1 pt-1 text-black ">
         <span className="font-bold text-xs "> HK$ </span>
         <span className="font-oswald font-black text-sm ">
