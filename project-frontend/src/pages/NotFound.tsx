aca3db2560a67ad07d91d955d6fc7fc4b25b4159
beeeeeeen9677 <benlau1234567@gmail.com>
2025-12-30 03:53:57 +0800

    added home btn to not found page

--- project-frontend/src/pages/NotFound.tsx
+++ project-frontend/src/pages/NotFound.tsx
@@ -1,12 +1,19 @@
+import { Link } from "react-router";
 import Header from "./../components/Header";
+import { useTranslation } from "../i18n/hooks";
 
 function NotFound() {
+  const { t } = useTranslation("common");
+
   return (
-    <div className="animate-fade-in min-h-screen ">
+    <div className="animate-fade-in min-h-screen flex flex-col items-center">
       <title>NOT FOUND | PREMIUM BEN TOYS</title>
       <Header />
-      <div className="flex justify-center items-center h-100">
-        <div className="text-5xl text">404 Not Found</div>
+      <div className="flex flex-col justify-center items-center h-full flex-1 gap-6">
+        <div className="text-5xl text font-oswald">404 Not Found</div>
+        <Link className="tw-round-primary-btn" to="/">
+          {t("buttons.home")}
+        </Link>
       </div>
     </div>
   );
